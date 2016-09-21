2f5e6b0451a312844392b31e4fa8c8258f28dd08
marvin <marvin.harootoonyan.570@my.csun.edu>
2016-09-21 02:28:50 -0700

    removed unecessary midi files kept only MidiListener interface because thats all we need for now and all virtual functions inside midilistener except noteOn and noteOff

--- app/src/main/java/com/csun_comp380_15884/group3/synthesizerapp/midi/MidiListener.java
+++ app/src/main/java/com/csun_comp380_15884/group3/synthesizerapp/midi/MidiListener.java
@@ -24,38 +24,4 @@ public interface MidiListener {
   // Control events.
   void onNoteOff(int channel, int note, int velocity);
   void onNoteOn(int channel, int note, int velocity);
-  void onNoteAftertouch(int channel, int note, int aftertouch);
-  void onController(int channel, int control, int value);
-  void onProgramChange(int channel, int program);
-  void onChannelAftertouch(int channel, int aftertouch);
-  void onPitchBend(int channel, int value);
-
-  // Other events.
-  void onTimingClock();
-  void onActiveSensing();
-
-  // Meta events.
-  // The events that have "byte[] text" are _probably_ encoded as ISO-8859-1.
-  void onSequenceNumber(int sequenceNumber);
-  void onText(byte[] text);
-  void onCopyrightNotice(byte[] text);
-  void onSequenceName(byte[] text);
-  void onInstrumentName(byte[] text);
-  void onLyrics(byte[] text);
-  void onMarker(byte[] text);
-  void onCuePoint(byte[] text);
-  void onChannelPrefix(int channel);
-  void onPort(byte[] data);  // TODO(klimt):  Decode this.
-  void onEndOfTrack();
-  void onSetTempo(int microsecondsPerQuarterNote);
-  void onSmpteOffset(byte[] data);  // TODO(klimt):  Decode this.
-  void onTimeSignature(int numerator,
-                       int denominator,
-                       int metronomePulse,
-                       int thirtySecondNotesPerQuarterNote);
-  void onKeySignature(int key, boolean isMinor);
-  void onSequencerSpecificEvent(byte[] data);
-
-  // SysEx events.
-  void onSysEx(byte[] data);
 }
